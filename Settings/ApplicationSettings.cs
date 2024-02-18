e33b80298e8d5834ba044ecc7363ca7018b320fa
georgebjork <georgebjork@outlook.com>
2024-02-18 15:57:31 -0800

    remove old blazor server for mvc

--- Settings/ApplicationSettings.cs
+++
@@ -1,6 +0,0 @@
-namespace LeviathanRipBlog.Settings;
-
-public static class ApplicationSettings {
-    public const string APPLICATION_NAME = "LeviathanRipBlog";
-    public const string SQL_CONNECTION_NAME = "DbConnection";
-}
