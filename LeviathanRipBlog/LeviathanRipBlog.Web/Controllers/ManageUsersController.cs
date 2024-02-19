04d0d11ce988d2dd9988ee634a0879b29cf7fcff
georgebjork <georgebjork@outlook.com>
2024-02-18 20:29:19 -0800

    add infra to add remove and promote users

--- LeviathanRipBlog/LeviathanRipBlog.Web/Controllers/ManageUsersController.cs
+++ LeviathanRipBlog/LeviathanRipBlog.Web/Controllers/ManageUsersController.cs
@@ -44,4 +44,6 @@ public class ManageUsersController : BaseAuthorizedController {
         
         return View(vm);
     }
+    
+    
 }
--- LeviathanRipBlog/LeviathanRipBlog.Web/Services/ManageUsers/ManageUsersService.cs
+++ LeviathanRipBlog/LeviathanRipBlog.Web/Services/ManageUsers/ManageUsersService.cs
@@ -1,6 +1,8 @@
 using LeviathanRipBlog.Data.Repositories;
 using LeviathanRipBlog.Web.Data;
 using LeviathanRipBlog.Web.Data.Models;
+using LeviathanRipBlog.Web.Settings;
+using Microsoft.AspNetCore.Identity;
 namespace LeviathanRipBlog.Web.Services.ManageUsers;
 
 public interface IManageUsersService {
@@ -9,14 +11,19 @@ public interface IManageUsersService {
     // User Invites
     Task<List<user_invitation>> GetInvites();
     Task<user_invitation?> GetInvite(string inviteId);
+
+    Task<ApplicationUser> PromoteAdmin(string userId);
+    Task<ApplicationUser> RevokeAdmin(string userId);
 }
 
 public class ManageUsersService : IManageUsersService {
     
     private readonly IUserRepository userRepository;
+    private readonly UserManager<ApplicationUser> userManager;
     
-    public ManageUsersService(IUserRepository userRepository) {
+    public ManageUsersService(IUserRepository userRepository, UserManager<ApplicationUser> userManager) {
         this.userRepository = userRepository;
+        this.userManager = userManager;
     }
     
     public async Task<List<ApplicationUser>> GetUsers() {
@@ -29,5 +36,47 @@ public class ManageUsersService : IManageUsersService {
     
     public async Task<user_invitation?> GetInvite(string inviteId) {
         return await userRepository.GetInvite(inviteId);
+    }
+    
+    public async Task<ApplicationUser> PromoteAdmin(string userId)
+    {
+        var user = await userManager.FindByIdAsync(userId);
+
+        if (user == null) throw new NullReferenceException("User not found");
+        
+        // Add to role
+        if (await userManager.IsInRoleAsync(user, Roles.ADMIN)) return user;
+        
+        await userManager.AddToRoleAsync(user, Roles.ADMIN);
+        user.IsAdmin = true;
+
+        return user;
+    }
+
+    public async Task<ApplicationUser> RevokeAdmin(string userId)
+    {
+        var user = await userManager.FindByIdAsync(userId);
+
+        if (user == null) throw new NullReferenceException("User not found");
+        
+        // Remove from role and make sure they're in the user role
+        await userManager.RemoveFromRoleAsync(user, Roles.ADMIN);
+        
+        if(!await userManager.IsInRoleAsync(user, Roles.USER))
+        {
+            await userManager.AddToRoleAsync(user, Roles.USER);
+        }
+        
+        user.IsAdmin = false;
+        return user;
+    }
+
+    private async Task RemoveUser(string userId)
+    {
+        var user = await userManager.FindByIdAsync(userId);
+        
+        if (user is null) throw new NullReferenceException("User not found"); 
+        
+        await userManager.DeleteAsync(user);
     }
 }
