26e6272bc4880f2ca409f805d40474142cc64327
George <georgebjork@outlook.com>
2024-04-02 20:14:16 -0700

    show only ipv4

--- LeviathanRipBlog/LeviathanRipBlog.Web/Middleware/RequestLogMiddleware.cs
+++ LeviathanRipBlog/LeviathanRipBlog.Web/Middleware/RequestLogMiddleware.cs
@@ -13,7 +13,7 @@ public class RequestLogMiddleware(RequestDelegate next, IUsernameRetriever usern
             path = context.Request.Path,
             method = context.Request.Method,
             timestamp = DateTime.UtcNow,
-            ip_address = context.Connection.RemoteIpAddress?.ToString(),
+            ip_address = GetIpAddress(context),
             user_agent = context.Request.Headers["User-Agent"].FirstOrDefault(),
             username = usernameRetriever.Username
         };
@@ -21,6 +21,27 @@ public class RequestLogMiddleware(RequestDelegate next, IUsernameRetriever usern
         await repository.Insert(requestLog);
         
         await next(context);
+    }
+
+    private string? GetIpAddress(HttpContext context)
+    {
+        var ipAddress = context.Connection.RemoteIpAddress;
+        string? ipv4Address = null;
+
+        if (ipAddress != null)
+        {
+            return ipAddress.AddressFamily switch
+            {
+                // If the address is IPv6 and is an IPv4-mapped IPv6 address, convert it to IPv4.
+                System.Net.Sockets.AddressFamily.InterNetworkV6 when ipAddress.IsIPv4MappedToIPv6 => ipAddress
+                    .MapToIPv4()
+                    .ToString(),
+                // Directly take the IPv4 address.
+                System.Net.Sockets.AddressFamily.InterNetwork => ipAddress.ToString(), _ => null
+            };
+        }
+
+        return ipv4Address;
     }
 }
 
